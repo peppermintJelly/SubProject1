cca8fee6cae43c6df18ae5f65d4a133a8675b9fd
Jenny Wu <jwu@dexcom.com>
2016-01-25 10:08:49 -0800

    Updating umbrella header file

--- SubProject1/SubProject1/SubProject1.h
+++ SubProject1/SubProject1/SubProject1.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char SubProject1VersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SubProject1/PublicHeader.h>
 
 
+#import <SubProject1/SubProject1_OrangeLabel.h>
