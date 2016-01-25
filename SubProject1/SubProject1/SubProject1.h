0fd6cc3b08e528ac5a58e82659ccaa92a56134f2
Jenny Wu <jwu@dexcom.com>
2016-01-25 08:17:53 -0800

    Commit subproject files

---
+++ SubProject1/SubProject1/SubProject1.h
@@ -0,0 +1,19 @@
+//
+//  SubProject1.h
+//  SubProject1
+//
+//  Created by Jenny Wu on 1/25/16.
+//  Copyright © 2016 Jenny Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubProject1.
+FOUNDATION_EXPORT double SubProject1VersionNumber;
+
+//! Project version string for SubProject1.
+FOUNDATION_EXPORT const unsigned char SubProject1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubProject1/PublicHeader.h>
+
+
