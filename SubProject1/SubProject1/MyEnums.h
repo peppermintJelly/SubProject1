32c752bec58e1533e522bb6bc33ea3ea2ed199ef
Jenny Wu <jwu@dexcom.com>
2016-01-25 08:49:57 -0800

    Adding enum to SUBPROJECT1

---
+++ SubProject1/SubProject1/MyEnums.h
@@ -0,0 +1,17 @@
+//
+//  MyEnums.h
+//  SubProject1
+//
+//  Created by Jenny Wu on 1/25/16.
+//  Copyright © 2016 Jenny Wu. All rights reserved.
+//
+
+#ifndef MyEnums_h
+#define MyEnums_h
+typedef enum (NSInteger, MySubProject1Enum){
+    MySubProject1Enum1,
+    MySubProject1Enum2,
+    MySubProject1Enum3,
+};
+
+#endif /* MyEnums_h */
