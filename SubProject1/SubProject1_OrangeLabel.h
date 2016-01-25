abec6b53395b16cc0ab734aceb575f08f205b2fb
Jenny Wu <jwu@dexcom.com>
2016-01-25 09:35:22 -0800

    Added Custom label to SubProject1

---
+++ SubProject1/SubProject1_OrangeLabel.h
@@ -0,0 +1,13 @@
+//
+//  SubProject1_OrangeLabel.h
+//  SubProject1
+//
+//  Created by Jenny Wu on 1/25/16.
+//  Copyright © 2016 Jenny Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubProject1_OrangeLabel : UILabel
+
+@end
---
+++ SubProject1/SubProject1_OrangeLabel.m
@@ -0,0 +1,21 @@
+//
+//  SubProject1_OrangeLabel.m
+//  SubProject1
+//
+//  Created by Jenny Wu on 1/25/16.
+//  Copyright © 2016 Jenny Wu. All rights reserved.
+//
+
+#import "SubProject1_OrangeLabel.h"
+
+@implementation SubProject1_OrangeLabel
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor orangeColor];
+        self.textColor = [UIColor darkGrayColor];
+    }
+    return self;
+}
+@end
